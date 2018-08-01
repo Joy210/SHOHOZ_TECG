e518d4485146a9b530694177bfec00c669cfe9b5
Md Sohanur Rahman JoY <md.sohanur.rahman.joy@gmail.com>
2018-08-01 15:18:00 +0600

    commit

--- Profile/editImage.js
+++ Profile/editImage.js
@@ -1,38 +1,13 @@
-(function () {
-    var uploader = document.createElement('input'),
-      image = document.getElementById('img-result');
-  
-    uploader.type = 'file';
-    uploader.accept = 'image/*';
-  
-    image.onclick = function() {
-      uploader.click();
-    }
-  
-    uploader.onchange = function() {
-      var reader = new FileReader();
-      reader.onload = function(evt) {
-        image.classList.remove('no-image');
-        image.style.backgroundImage = 'url(' + evt.target.result + ')';
-        var request = {
-          itemtype: 'test 1',
-          brand: 'test 2',
-          images: [{
-            data: evt.target.result
-          }]
-        };
-        
-        document.querySelector('.show-button').style.display = 'block';
-        document.querySelector('.upload-result__content').innerHTML = JSON.stringify(request, null, '  ');
-      }
-      reader.readAsDataURL(uploader.files[0]);
-    }
-    
-    document.querySelector('.hide-button').onclick = function () {
-      document.querySelector('.upload-result').style.display = 'none';
-    };
-    
-    document.querySelector('.show-button').onclick = function () {
-      document.querySelector('.upload-result').style.display = 'block';
-    };
-  })();
+$('.file-input').change(function(){
+  var curElement = $(this).parent().parent().find('.image');
+  console.log(curElement);
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      curElement.attr('src', e.target.result);
+  };
+
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});
