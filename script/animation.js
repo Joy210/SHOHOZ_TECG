5256889399c45ea0daa035c735ad35989b7760d6
Md Sohanur Rahman JoY <md.sohanur.rahman.joy@gmail.com>
2018-10-11 12:06:52 +0600

    animation

---
+++ script/animation.js
@@ -0,0 +1,49 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.showcase-header', {
+    duration: 2000,
+    origin: 'left',
+    distance: '500px'
+});
+sr.reveal('.article', {
+    duration: 2000,
+    origin: 'left',
+    distance: '200px'
+});
+sr.reveal('.box', {
+    duration: 2000,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('.powered-by', {
+    duration: 2000,
+    origin: 'top',
+    distance: '150px'
+});
+sr.reveal('.newsLetter', {
+    duration: 2000,
+    origin: 'left',
+    distance: '150px'
+});
+sr.reveal('.contact', {
+    duration: 2000,
+    origin: 'right',
+    distance: '150px'
+});
+
+sr.reveal('.postDetails', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+sr.reveal('.about', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('.social-link', {
+    duration: 2000,
+    origin: 'right',
+    distance: '150px'
+});
